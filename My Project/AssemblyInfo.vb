c57c702070edb58cbc44c9115869e1e294ab6693
Lane Coleman <lane.coleman@live.com>
2020-09-02 19:26:32 -0600

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SimpleCalculatorLC")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("SimpleCalculatorLC")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("cc5e8b73-8e28-443e-95f5-cc5a40593792")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ SimpleCalculatorLC.vb
@@ -0,0 +1,62 @@
+﻿Option Strict On
+Option Explicit On
+Option Compare Text
+'Lane Coleman
+'RCET0265
+'Fall 2020
+'Simple Calculator
+'
+
+Module SimpleCalculatorLC
+
+    Sub Main()
+        Dim firstInput, secondInput, userResponse As String
+        Dim firstNumber, secondNumber As Integer
+        Dim goodData As Boolean
+        Do
+            Console.WriteLine("Enter q at any time to quit." & vbNewLine)
+            Do
+                Try
+                    Console.WriteLine("Give me a number")
+                    firstInput = Console.ReadLine()
+                    firstNumber = CInt(firstInput)
+                    Console.WriteLine("Give me another number")
+                    secondInput = Console.ReadLine()
+                    secondNumber = CInt(secondInput)
+                    goodData = True
+                Catch ex As Exception
+                    If firstInput = "q" Or secondInput = "q" Then
+                        Exit Sub
+                    End If
+                    Console.WriteLine("I asked for a number! Try again.")
+                    goodData = False
+                End Try
+
+            Loop Until goodData = True
+
+            Do
+                Console.WriteLine("add or multiply?")
+                userResponse = Console.ReadLine()
+                If userResponse = "q" Then
+                    Exit Sub
+                ElseIf userResponse = "add" Then
+                    Console.WriteLine(firstNumber + secondNumber)
+
+                ElseIf userResponse = "multiply" Then
+                    Console.WriteLine(firstNumber * secondNumber)
+
+                ElseIf userResponse <> "multiply" And userResponse <> "add" Then
+                    Console.WriteLine("Only multiply or add will work")
+
+
+
+                End If
+            Loop Until userResponse = "add" Or userResponse = "multiply"
+            Console.ReadLine()
+            Console.Clear()
+
+        Loop
+
+    End Sub
+
+End Module
